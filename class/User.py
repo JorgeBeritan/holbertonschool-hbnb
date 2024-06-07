808cb3cc151898369fca73f585ad799615c9d389
NicoBoni55 <bonifrank55@gmail.com>
2024-06-07 09:43:51 -0300

    User

--- class/User.py
+++ class/User.py
@@ -5,4 +5,10 @@ definimos las clases
 
 
 class User():
-    
+    def __init__(self, ID, Email, First_name, Last_name, Create_at, Update_at):
+        self.ID = ID
+        self.Email = Email
+        self.First_name = First_name
+        self.Last_name = Last_name
+        self.Create_at = Create_at
+        self.Update_at = Update_at
