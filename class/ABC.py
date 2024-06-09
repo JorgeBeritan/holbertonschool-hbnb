ebcfd40273f87a8da7d63b03c5d827c99f356c6f
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-06-09 11:35:27 +0000

    Abstractclass

---
+++ class/ABC.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+from uuid import uuid4
+
+class BaseClass(ABC):
+
+    def __init__(self):
+        self.__id = uuid4()
+        self.__create_at = datetime.now()
+        self.__update_at = datetime.now()
+
+    def set_time(func):
+        def wrapper_set_time(self, *args, **kwargs):
+            self.__update_at = datetime.now()
+            return func(self, *args, **kwargs)
+        return wrapper_set_time
+    
