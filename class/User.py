32d0220dce85f32927ddb9b7dec7ac1b943f9d4b
JorgeBeritan <159051280+JorgeBeritan@users.noreply.github.com>
2024-06-11 20:40:35 +0000

    Implement uuid in Place

--- class/User.py
+++ class/User.py
@@ -2,32 +2,32 @@
 """
 definimos las clases
 """
-
+import uuid
 
 class User():
-    def __init__(self, ID, Email, First_name, Last_name, Create_at, Update_at):
-        self.ID = ID
+    def __init__(self, Email, First_name, Last_name):
+        self.ID = uuid.uuid4()
         self.Email = Email
         self.First_name = First_name
         self.Last_name = Last_name
-        self.Create_at = Create_at
-        self.Update_at = Update_at
+        #self.Create_at = Create_at
+        #self.Update_at = Update_at
 
     @property
     def ID(self):
-        return self.ID
+        return self.__ID
     
     @property
     def Email(self):
-        return self.Email
+        return self.__Email
     
     @property
     def First_name(self):
-        return self.First_name
+        return self.__First_name
     
     @property
     def Last_name(self):
-        return self.Last_name
+        return self.__Last_name
     
     @property
     def create_at(self):
@@ -37,15 +37,22 @@ class User():
     def update_at(self):
         return self.__update_at
     
+    @ID.setter
+    def ID(self, ID):
+        self.__ID = ID
+    
     @Email.setter
     def Email(self, Email):
-        pass
+        if type(Email) is not str:
+            raise TypeError("The email user must be a string")
+        self.__Email = Email
+
 
     @First_name.setter
     def First_name(self, First_name):
         if type(First_name) is not str:
             raise TypeError("First name must be a string")
-        elif First_name is None:
+        elif First_name == "":
             raise ValueError("First name is requerid")
         self.__First_name = First_name
 
@@ -53,6 +60,6 @@ class User():
     def Last_name(self, Last_name):
         if type(Last_name) is not str:
             raise TypeError("Last name must be a string")
-        elif Last_name is None:
+        elif Last_name == "":
             raise ValueError("Last name is requerid")
         self.__Last_name = Last_name
